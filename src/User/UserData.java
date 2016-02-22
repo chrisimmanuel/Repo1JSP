f8fa313fa5b0d0e9b6c1568b9b317b0267e4a2ef
chrisimmanuel <hutagalung.chris@digitalkomputasiimmanuel.co.id>
2016-02-22 14:55:37 +0700

    Commit1

---
+++ src/User/UserData.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) PT. Digital Komputasi Immanuel.
+ */
+
+package User;
+
+/**
+ * Created by DIGIKOM_EX1 on 2/22/2016.
+ */
+public class UserData {
+
+    String name;
+    String email;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
